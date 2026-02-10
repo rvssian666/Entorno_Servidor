b8761c0b899cb532efcfecf48906d5bfc1b89480
Alexander <rvssian66@hotmail.com>
2026-02-10 20:16:49 +0100

    agregando los settings y urls

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -5,5 +5,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # aqui pongo la url para la vista de tareas,
+    path('',views.get_all_list,name='all_list'),
     path('list/',views.get_all_list,name='all_list'),
 ]
