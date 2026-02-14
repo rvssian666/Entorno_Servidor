8dc40112794c0f704cfa6dcc514493ab1861f2da
Alexander <rvssian66@hotmail.com>
2026-02-14 16:14:14 +0100

    view añadidas en el index

--- task_manager/task_manager/views.py
+++ task_manager/task_manager/views.py
@@ -10,8 +10,6 @@ def get_all_list(request):
             new_list = TaskList(name=name)
             new_list.save()
             return redirect('all_list') 
-
-    # Esto se ejecuta en el GET o después del redirect
     all_list = TaskList.objects.all()
     return render(request, 'index.html', {'all_list': all_list})
 
@@ -20,27 +18,37 @@ def get_list(request,id):
     all_list=TaskList.objects.get(id=id)
     return render(request,'index.html',{'all_list':all_list})
 
-# Eliminar  de tareas
+# Eliminar lista tareas
 def delete_task_list(request,list_id):
  list=get_object_or_404(TaskList,id=list_id)
  list.delete()
  return redirect('all_list')
 
-# Obtener tareas de una lista
+
 def get_task(request, lista_id):
     task_list = get_object_or_404(TaskList, id=lista_id)
+    
+    # Si el usuario envía el formulario (POST)
     if request.method == 'POST':
         title = request.POST.get('title')
         if title:
-            new_task = Task(title=title)
-            new_task.save()
-            return redirect('get_task', lista_id=lista_id)  
-    tasks = task_list.task_set.all()
-    return render(request, 'task.html', {
-        'tasks': tasks, 
-        'task_list': task_list
-    })
+            Task.objects.create(title=title, task_list=task_list)
+            return redirect('get_task', lista_id=lista_id)
 
-   
+    # Si el usuario solo entra a ver (GET)
+    tasks = task_list.task_set.all() 
+    context={
+        'tasks':tasks,
+        'task_list':task_list
+    }
+    return render(request, 'task.html', context)
+
+
+
+def delete_especific_task(request,task_id,lista_id):
+ task=get_object_or_404(Task,id=task_id)
+ id_deleted=task.task_list.id
+ task.delete()
+ return redirect('get_task',lista_id=id_deleted)
 
 
