ce4d1d5fd6d10d30b478959578f84d4d1f920ce1
Alexander <rvssian66@hotmail.com>
2026-02-14 15:09:09 +0100

    url semicompletadas

--- task_manager/task_manager/urls.py
+++ task_manager/task_manager/urls.py
@@ -5,12 +5,12 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # aqui pongo la url para la vista de tareas,
     path('',views.get_all_list,name='all_list'),
     path('lists/',views.get_all_list,name='all_list'),
     path('lists/<list_id>/',views.delete_task_list,name='delete_task_list'),
     #para obtener o pinchar una tarea específica
     path('lists/<int:lista_id>/tasks/', views.get_task, name='get_task'),
+    path('lists/<int:lista_id>/tasks/<int:task_id>/', views.delete_especific_task, name='delete_especific_task'),
     
     
 ]
